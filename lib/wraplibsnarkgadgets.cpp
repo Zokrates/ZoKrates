9c9bf284ed9ead6284cba9a0a0aeac99fc0bb492
Your Name <you@example.com>
2018-05-13 14:37:04 +0100

    fixed _sha256Constraints

--- lib/wraplibsnarkgadgets.cpp
+++ lib/wraplibsnarkgadgets.cpp
@@ -5,10 +5,32 @@
 
 #include <libsnark/zk_proof_systems/ppzksnark/r1cs_ppzksnark/r1cs_ppzksnark.hpp>
 
+
 #include <iostream>
 
 using namespace libsnark;
 using namespace libff;
+
+void constraint_to_json(linear_combination<FieldT> constraints, std::stringstream &ss)
+{
+    ss << "{";
+    uint count = 0;
+    for (const linear_term<FieldT>& lt : constraints.terms)
+    {
+        if (count != 0) {
+            ss << ",";
+        }
+        if (lt.coeff != 0 && lt.coeff != 1) {
+            ss << '"' << lt.index << '"' << ":" << "-1";
+        }
+        else {
+            ss << '"' << lt.index << '"' << ":" << lt.coeff;
+        }
+        count++;
+    }
+    ss << "}";
+}
+
 
 std::string r1cs_to_json(protoboard<FieldT> pb, uint input_variables)
 {
@@ -16,7 +38,7 @@ std::string r1cs_to_json(protoboard<FieldT> pb, uint input_variables)
     // exists. Having trouble setting that up so will leave for now.
     r1cs_constraint_system<FieldT> constraints = pb.get_constraint_system();
     std::stringstream ss;
-    ss << "\n{\"variables\":[";
+    /*ss << "\n{\"variables\":[";
 
     for (size_t i = 0; i < input_variables + 1; ++i)
     {
@@ -27,15 +49,15 @@ std::string r1cs_to_json(protoboard<FieldT> pb, uint input_variables)
     }
     ss << "],\n";
     ss << "\"constraints\":[";
-
+    */
     for (size_t c = 0; c < constraints.num_constraints(); ++c)
     {
         ss << "[";// << "\"A\"=";
-        // constraint_to_json(constraints.constraints[c].a, ss);
+        constraint_to_json(constraints.constraints[c].a, ss);
         ss << ",";// << "\"B\"=";
-        // constraint_to_json(constraints.constraints[c].b, ss);
+        constraint_to_json(constraints.constraints[c].b, ss);
         ss << ",";// << "\"A\"=";;
-        // constraint_to_json(constraints.constraints[c].c, ss);
+        constraint_to_json(constraints.constraints[c].c, ss);
         if (c == constraints.num_constraints()-1 ) {
             ss << "]\n";
         } else {
@@ -49,18 +71,19 @@ std::string r1cs_to_json(protoboard<FieldT> pb, uint input_variables)
 
 void _sha256Constraints()
 {
-    // block_variable<FieldT> input;
-    // block_variable<FieldT> output;
-    // auto hash = std::make_shared<sha256_ethereum>(pb, 256, input; output, "cm_hash");
-
-    // std::shared_ptr<sha256_ethereum> hash;
     protoboard<FieldT> pb;
-    
-    // hash.reset(new sha256_ethereum(
-    //     pb, 256, input, output, "cm_hash"
-    // ));
-    // hash->generate_r1cs_constraints(true);
+    block_variable<FieldT> input(pb, 256, "input");
+    digest_variable<FieldT> output(pb, 256, "output");
+    //auto hash = std::make_shared<sha256_ethereum>(pb, 256, input, output, "hash");
 
+    std::shared_ptr<sha256_ethereum> hash;
+
+    
+    hash.reset(new sha256_ethereum(
+        pb, 256, input, output, "cm_hash"
+    ));
+    hash->generate_r1cs_constraints(true);
+    r1cs_to_json(pb, 0);
     // return(r1cs_to_json(pb, 10));
     //return "";
 }
@@ -83,9 +106,7 @@ std::string array_to_json(protoboard<FieldT> pb)
     return(ss.str());
 }
 
-
-
-std::string _sha256Witness( pb_variable_array<FieldT> left, pb_variable_array<FieldT> right)
+std::string _sha256Witness( std::vector<bool> left, std::vector<bool> right)
 {
     protoboard<FieldT> pb;
     std::shared_ptr<sha256_ethereum> hash;
@@ -93,15 +114,14 @@ std::string _sha256Witness( pb_variable_array<FieldT> left, pb_variable_array<Fi
     pb_variable<FieldT> ZERO;
     ZERO.allocate(pb, "ZERO");
     pb.val(ZERO) = 0;
+   
+    digest_variable<FieldT> output(pb, 256, "output");
 
-    pb.set_input_sizes(90);
-
-    // block_variable<FieldT> output;
-    // block_variable<FieldT> input =  block_variable<FieldT>(pb, {
-    //     from_bits(left, ZERO),
-    //     from_bits(right, ZERO)
-    // }, "input_variable");
-
+   
+   //block_variable<FieldT> input =  block_variable<FieldT>(pb, {
+   //     from_bits(left, ZERO),
+   //     from_bits(right, ZERO)
+   // }, "input_variable");
 
     // hash.reset(new sha256_ethereum(
     //     pb, 256, input, output, "cm_hash"
