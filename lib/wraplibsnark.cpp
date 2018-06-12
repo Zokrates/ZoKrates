50b3711ba33ea2327f8d472771ced0706c3bae1f
schaeff <thibaut@schaeff.fr>
2018-06-12 18:58:20 +0200

    adapt verification key and proof

--- lib/wraplibsnark.cpp
+++ lib/wraplibsnark.cpp
@@ -14,7 +14,7 @@
 // contains definition of alt_bn128 ec public parameters
 #include "libff/algebra/curves/alt_bn128/alt_bn128_pp.hpp"
 // contains required interfaces and types (keypair, proof, generator, prover, verifier)
-#include <libsnark/zk_proof_systems/ppzksnark/r1cs_ppzksnark/r1cs_ppzksnark.hpp>
+#include <libsnark/zk_proof_systems/ppzksnark/r1cs_gg_ppzksnark/r1cs_gg_ppzksnark.hpp>
 
 typedef long integer_coeff_t;
 
@@ -73,10 +73,22 @@ std::string outputPointG2AffineAsHex(libff::alt_bn128_G2 _p)
                 HexStringFromLibsnarkBigint(aff.Y.c0.as_bigint()) + "]";
 }
 
+// std::string outputPointGTAffineAsHex(libff::alt_bn128_GT _p)
+// {
+//         libff::alt_bn128_GT aff = _p;
+//         aff.to_affine_coordinates();
+//         return
+//                 "[0x" +
+//                 HexStringFromLibsnarkBigint(aff.X.c1.as_bigint()) + ", 0x" +
+//                 HexStringFromLibsnarkBigint(aff.X.c0.as_bigint()) + "], [0x" +
+//                 HexStringFromLibsnarkBigint(aff.Y.c1.as_bigint()) + ", 0x" +
+//                 HexStringFromLibsnarkBigint(aff.Y.c0.as_bigint()) + "]";
+// }
+
 //takes input and puts it into constraint system
-r1cs_ppzksnark_constraint_system<libff::alt_bn128_pp> createConstraintSystem(const uint8_t* A, const uint8_t* B, const uint8_t* C, int constraints, int variables, int inputs)
+r1cs_gg_ppzksnark_constraint_system<libff::alt_bn128_pp> createConstraintSystem(const uint8_t* A, const uint8_t* B, const uint8_t* C, int constraints, int variables, int inputs)
 {
-  r1cs_ppzksnark_constraint_system<libff::alt_bn128_pp> cs;
+  r1cs_gg_ppzksnark_constraint_system<libff::alt_bn128_pp> cs;
   cs.primary_input_size = inputs;
   cs.auxiliary_input_size = variables - inputs - 1; // ~one not included
 
@@ -118,9 +130,9 @@ r1cs_ppzksnark_constraint_system<libff::alt_bn128_pp> createConstraintSystem(con
 }
 
 // keypair generateKeypair(constraints)
-r1cs_ppzksnark_keypair<libff::alt_bn128_pp> generateKeypair(const r1cs_ppzksnark_constraint_system<libff::alt_bn128_pp> &cs){
-  // from r1cs_ppzksnark.hpp
-  return r1cs_ppzksnark_generator<libff::alt_bn128_pp>(cs);
+r1cs_gg_ppzksnark_keypair<libff::alt_bn128_pp> generateKeypair(const r1cs_gg_ppzksnark_constraint_system<libff::alt_bn128_pp> &cs){
+  // from r1cs_gg_ppzksnark.hpp
+  return r1cs_gg_ppzksnark_generator<libff::alt_bn128_pp>(cs);
 }
 
 template<typename T>
@@ -153,32 +165,28 @@ T loadFromFile(std::string path) {
     return obj;
 }
 
-void serializeProvingKeyToFile(r1cs_ppzksnark_proving_key<libff::alt_bn128_pp> pk, const char* pk_path){
+void serializeProvingKeyToFile(r1cs_gg_ppzksnark_proving_key<libff::alt_bn128_pp> pk, const char* pk_path){
   writeToFile(pk_path, pk);
 }
 
-r1cs_ppzksnark_proving_key<libff::alt_bn128_pp> deserializeProvingKeyFromFile(const char* pk_path){
-  return loadFromFile<r1cs_ppzksnark_proving_key<libff::alt_bn128_pp>>(pk_path);
+r1cs_gg_ppzksnark_proving_key<libff::alt_bn128_pp> deserializeProvingKeyFromFile(const char* pk_path){
+  return loadFromFile<r1cs_gg_ppzksnark_proving_key<libff::alt_bn128_pp>>(pk_path);
 }
 
-void serializeVerificationKeyToFile(r1cs_ppzksnark_verification_key<libff::alt_bn128_pp> vk, const char* vk_path){
+void serializeVerificationKeyToFile(r1cs_gg_ppzksnark_verification_key<libff::alt_bn128_pp> vk, const char* vk_path){
   std::stringstream ss;
 
-  unsigned icLength = vk.encoded_IC_query.rest.indices.size() + 1;
-
-  ss << "\t\tvk.A = " << outputPointG2AffineAsHex(vk.alphaA_g2) << endl;
-  ss << "\t\tvk.B = " << outputPointG1AffineAsHex(vk.alphaB_g1) << endl;
-  ss << "\t\tvk.C = " << outputPointG2AffineAsHex(vk.alphaC_g2) << endl;
+  unsigned icLength = vk.gamma_ABC_g1.rest.indices.size() + 1;
+  
+  ss << "\t\tvk.ab = Pairing.GTPoint(" << "TODO" << ");" << endl; // haven't found way to serialize this member yet
   ss << "\t\tvk.gamma = " << outputPointG2AffineAsHex(vk.gamma_g2) << endl;
-  ss << "\t\tvk.gammaBeta1 = " << outputPointG1AffineAsHex(vk.gamma_beta_g1) << endl;
-  ss << "\t\tvk.gammaBeta2 = " << outputPointG2AffineAsHex(vk.gamma_beta_g2) << endl;
-  ss << "\t\tvk.Z = " << outputPointG2AffineAsHex(vk.rC_Z_g2) << endl;
-  ss << "\t\tvk.IC.len() = " << icLength << endl;
-  ss << "\t\tvk.IC[0] = " << outputPointG1AffineAsHex(vk.encoded_IC_query.first) << endl;
+  ss << "\t\tvk.delta = " << outputPointG2AffineAsHex(vk.delta_g2) << endl;
+  ss << "\t\tvk.gammaABC.len() = " << icLength << ");" << endl;
+  ss << "\t\tvk.gammaABC[0] = " << outputPointG1AffineAsHex(vk.gamma_ABC_g1.first) << endl;
   for (size_t i = 1; i < icLength; ++i)
   {
-                  auto vkICi = outputPointG1AffineAsHex(vk.encoded_IC_query.rest.values[i - 1]);
-                  ss << "\t\tvk.IC[" << i << "] = " << vkICi << endl;
+          auto vkICi = outputPointG1AffineAsHex(vk.gamma_ABC_g1.rest.values[i - 1]);
+          ss << "\t\tvk.IC[" << i << "] = " << vkICi << endl;
   }
 
   std::ofstream fh;
@@ -190,22 +198,19 @@ void serializeVerificationKeyToFile(r1cs_ppzksnark_verification_key<libff::alt_b
 }
 
 // compliant with solidty verification example
-void exportVerificationKey(r1cs_ppzksnark_keypair<libff::alt_bn128_pp> keypair){
-        unsigned icLength = keypair.vk.encoded_IC_query.rest.indices.size() + 1;
+void exportVerificationKey(r1cs_gg_ppzksnark_keypair<libff::alt_bn128_pp> keypair){
+
+        unsigned icLength = keypair.vk.gamma_ABC_g1.rest.indices.size() + 1;
 
         cout << "\tVerification key in Solidity compliant format:{" << endl;
-        cout << "\t\tvk.A = Pairing.G2Point(" << outputPointG2AffineAsHex(keypair.vk.alphaA_g2) << ");" << endl;
-        cout << "\t\tvk.B = Pairing.G1Point(" << outputPointG1AffineAsHex(keypair.vk.alphaB_g1) << ");" << endl;
-        cout << "\t\tvk.C = Pairing.G2Point(" << outputPointG2AffineAsHex(keypair.vk.alphaC_g2) << ");" << endl;
+        cout << "\t\tvk.ab = Pairing.GTPoint(" << "TODO" << ");" << endl; // haven't found way to serialize this member yet
         cout << "\t\tvk.gamma = Pairing.G2Point(" << outputPointG2AffineAsHex(keypair.vk.gamma_g2) << ");" << endl;
-        cout << "\t\tvk.gammaBeta1 = Pairing.G1Point(" << outputPointG1AffineAsHex(keypair.vk.gamma_beta_g1) << ");" << endl;
-        cout << "\t\tvk.gammaBeta2 = Pairing.G2Point(" << outputPointG2AffineAsHex(keypair.vk.gamma_beta_g2) << ");" << endl;
-        cout << "\t\tvk.Z = Pairing.G2Point(" << outputPointG2AffineAsHex(keypair.vk.rC_Z_g2) << ");" << endl;
-        cout << "\t\tvk.IC = new Pairing.G1Point[](" << icLength << ");" << endl;
-        cout << "\t\tvk.IC[0] = Pairing.G1Point(" << outputPointG1AffineAsHex(keypair.vk.encoded_IC_query.first) << ");" << endl;
+        cout << "\t\tvk.delta = Pairing.G2Point(" << outputPointG2AffineAsHex(keypair.vk.delta_g2) << ");" << endl;
+        cout << "\t\tvk.gammaABC = new Pairing.G1Point[](" << icLength << ");" << endl;
+        cout << "\t\tvk.gammaABC[0] = Pairing.G1Point(" << outputPointG1AffineAsHex(keypair.vk.gamma_ABC_g1.first) << ");" << endl;
         for (size_t i = 1; i < icLength; ++i)
         {
-                auto vkICi = outputPointG1AffineAsHex(keypair.vk.encoded_IC_query.rest.values[i - 1]);
+                auto vkICi = outputPointG1AffineAsHex(keypair.vk.gamma_ABC_g1.rest.values[i - 1]);
                 cout << "\t\tvk.IC[" << i << "] = Pairing.G1Point(" << vkICi << ");" << endl;
         }
         cout << "\t\t}" << endl;
@@ -223,16 +228,11 @@ void exportInput(r1cs_primary_input<libff::Fr<libff::alt_bn128_pp>> input){
 } 
 
 
-void printProof(r1cs_ppzksnark_proof<libff::alt_bn128_pp> proof){
-                cout << "Proof:"<< endl;
-                cout << "A = Pairing.G1Point(" << outputPointG1AffineAsHex(proof.g_A.g)<< ");" << endl;
-                cout << "A_p = Pairing.G1Point(" << outputPointG1AffineAsHex(proof.g_A.h)<< ");" << endl;
-                cout << "B = Pairing.G2Point(" << outputPointG2AffineAsHex(proof.g_B.g)<< ");" << endl;
-                cout << "B_p = Pairing.G1Point(" << outputPointG1AffineAsHex(proof.g_B.h)<<");" << endl;
-                cout << "C = Pairing.G1Point(" << outputPointG1AffineAsHex(proof.g_C.g)<< ");" << endl;
-                cout << "C_p = Pairing.G1Point(" << outputPointG1AffineAsHex(proof.g_C.h)<<");" << endl;
-                cout << "H = Pairing.G1Point(" << outputPointG1AffineAsHex(proof.g_H)<<");"<< endl;
-                cout << "K = Pairing.G1Point(" << outputPointG1AffineAsHex(proof.g_K)<<");"<< endl;
+void printProof(r1cs_gg_ppzksnark_proof<libff::alt_bn128_pp> proof){
+    cout << "Proof:"<< endl;
+    cout << "A = Pairing.G1Point(" << outputPointG1AffineAsHex(proof.g_A)<< ");" << endl;
+    cout << "B = Pairing.G2Point(" << outputPointG2AffineAsHex(proof.g_B)<< ");" << endl;
+    cout << "C = Pairing.G1Point(" << outputPointG1AffineAsHex(proof.g_C)<< ");" << endl;
 }
 
 bool _setup(const uint8_t* A, const uint8_t* B, const uint8_t* C, int constraints, int variables, int inputs, const char* pk_path, const char* vk_path)
@@ -250,7 +250,7 @@ bool _setup(const uint8_t* A, const uint8_t* B, const uint8_t* C, int constraint
   assert(cs.num_constraints() == (unsigned)constraints);
 
   // create keypair
-  auto keypair = r1cs_ppzksnark_generator<libff::alt_bn128_pp>(cs);
+  auto keypair = r1cs_gg_ppzksnark_generator<libff::alt_bn128_pp>(cs);
 
   // Export vk and pk to files
   serializeProvingKeyToFile(keypair.pk, pk_path);
@@ -291,7 +291,7 @@ bool _generate_proof(const char* pk_path, const uint8_t* public_inputs, int publ
   // cout << "auxiliary input:"<< endl << auxiliary_input;
 
   // Proof Generation
-  auto proof = r1cs_ppzksnark_prover<libff::alt_bn128_pp>(pk, primary_input, auxiliary_input);
+  auto proof = r1cs_gg_ppzksnark_prover<libff::alt_bn128_pp>(pk, primary_input, auxiliary_input);
 
   // print proof
   printProof(proof);
