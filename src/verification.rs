4a74ac31222d280a8d6da15b533f32973cc6cfeb
schaeff <thibaut@schaeff.fr>
2018-06-13 14:25:10 +0200

    update verification contract

--- src/verification.rs
+++ src/verification.rs
@@ -144,80 +144,50 @@ library Pairing {
 contract Verifier {
     using Pairing for *;
     struct VerifyingKey {
-        Pairing.G2Point A;
-        Pairing.G1Point B;
-        Pairing.G2Point C;
-        Pairing.G2Point gamma;
-        Pairing.G1Point gammaBeta1;
-        Pairing.G2Point gammaBeta2;
-        Pairing.G2Point Z;
-        Pairing.G1Point[] IC;
+        Pairing.G2Point ab;
+        Pairing.G1Point gamma;
+        Pairing.G2Point delta;
+        Pairing.G1Point[] gammaABC;
     }
     struct Proof {
         Pairing.G1Point A;
-        Pairing.G1Point A_p;
         Pairing.G2Point B;
-        Pairing.G1Point B_p;
         Pairing.G1Point C;
-        Pairing.G1Point C_p;
-        Pairing.G1Point K;
-        Pairing.G1Point H;
     }
     function verifyingKey() pure internal returns (VerifyingKey vk) {
-        vk.A = Pairing.G2Point(<%vk_a%>);
-        vk.B = Pairing.G1Point(<%vk_b%>);
-        vk.C = Pairing.G2Point(<%vk_c%>);
-        vk.gamma = Pairing.G2Point(<%vk_g%>);
-        vk.gammaBeta1 = Pairing.G1Point(<%vk_gb1%>);
-        vk.gammaBeta2 = Pairing.G2Point(<%vk_gb2%>);
-        vk.Z = Pairing.G2Point(<%vk_z%>);
-        vk.IC = new Pairing.G1Point[](<%vk_ic_length%>);
-        <%vk_ic_pts%>
+        vk.ab = Pairing.G2Point(<%vk_ab%>);
+        vk.gamma = Pairing.G2Point(<%vk_gamma%>);
+        vk.delta = Pairing.G2Point(<%vk_delta%>);
+        vk.gammaABC = new Pairing.G1Point[](<%vk_gammaABC_length%>);
+        <%vk_gammaABC_pts%>
     }
     function verify(uint[] input, Proof proof) internal returns (uint) {
         VerifyingKey memory vk = verifyingKey();
-        require(input.length + 1 == vk.IC.length);
+        require(input.length + 1 == vk.gammaABC.length);
         // Compute the linear combination vk_x
         Pairing.G1Point memory vk_x = Pairing.G1Point(0, 0);
         for (uint i = 0; i < input.length; i++)
-            vk_x = Pairing.addition(vk_x, Pairing.scalar_mul(vk.IC[i + 1], input[i]));
-        vk_x = Pairing.addition(vk_x, vk.IC[0]);
-        if (!Pairing.pairingProd2(proof.A, vk.A, Pairing.negate(proof.A_p), Pairing.P2())) return 1;
-        if (!Pairing.pairingProd2(vk.B, proof.B, Pairing.negate(proof.B_p), Pairing.P2())) return 2;
-        if (!Pairing.pairingProd2(proof.C, vk.C, Pairing.negate(proof.C_p), Pairing.P2())) return 3;
-        if (!Pairing.pairingProd3(
-            proof.K, vk.gamma,
-            Pairing.negate(Pairing.addition(vk_x, Pairing.addition(proof.A, proof.C))), vk.gammaBeta2,
-            Pairing.negate(vk.gammaBeta1), proof.B
-        )) return 4;
-        if (!Pairing.pairingProd3(
-                Pairing.addition(vk_x, proof.A), proof.B,
-                Pairing.negate(proof.H), vk.Z,
-                Pairing.negate(proof.C), Pairing.P2()
-        )) return 5;
+            vk_x = Pairing.addition(vk_x, Pairing.scalar_mul(vk.gammaABC[i + 1], input[i]));
+        vk_x = Pairing.addition(vk_x, vk.gammaABC[0]);
+
+        if(!Pairing.pairingProd4(
+             proof.A, proof.B,
+             Pairing.negate(vk_x), vk.gamma,
+             Pairing.negate(proof.C), vk.delta,
+             Pairing.negate(Pairing.P1()), vk.ab)) return 1;
         return 0;
     }
     event Verified(string);
     function verifyTx(
             uint[2] a,
-            uint[2] a_p,
             uint[2][2] b,
-            uint[2] b_p,
             uint[2] c,
-            uint[2] c_p,
-            uint[2] h,
-            uint[2] k,
             uint[<%vk_input_length%>] input
         ) public returns (bool r) {
         Proof memory proof;
         proof.A = Pairing.G1Point(a[0], a[1]);
-        proof.A_p = Pairing.G1Point(a_p[0], a_p[1]);
         proof.B = Pairing.G2Point([b[0][0], b[0][1]], [b[1][0], b[1][1]]);
-        proof.B_p = Pairing.G1Point(b_p[0], b_p[1]);
         proof.C = Pairing.G1Point(c[0], c[1]);
-        proof.C_p = Pairing.G1Point(c_p[0], c_p[1]);
-        proof.H = Pairing.G1Point(h[0], h[1]);
-        proof.K = Pairing.G1Point(k[0], k[1]);
         uint[] memory inputValues = new uint[](input.length);
         for(uint i = 0; i < input.length; i++){
             inputValues[i] = input[i];
