86683ac18574d654c3145cbff84c5ae40cbd090b
schaeff <thibaut@schaeff.fr>
2018-11-08 18:11:26 +0100

    add circleci config

---
+++ build_libsnark.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export LIBSNARK_COMMIT=f7c87b88744ecfd008126d415494d9b34c4c1b20
+export LIBSNARK_SOURCE_PATH=.tmp/libsnark
+
+apt-get update && apt-get install -y --no-install-recommends \
+    ca-certificates \
+    build-essential \
+    cmake \
+    curl \
+    libboost-dev \
+    libboost-program-options-dev \
+    libgmp3-dev \
+    libprocps-dev \
+    libssl-dev \
+    pkg-config \
+    python-markdown \
+    git \
+    && rm -rf /var/lib/apt/lists/* \
+    && git clone https://github.com/scipr-lab/libsnark.git $LIBSNARK_SOURCE_PATH \
+    && git -C $LIBSNARK_SOURCE_PATH checkout $LIBSNARK_COMMIT \
+    && git -C $LIBSNARK_SOURCE_PATH submodule update --init --recursive \
