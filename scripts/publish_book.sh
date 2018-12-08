614b9f118a15b8d89317c059d875aa9b9e11fd2f
Stefan Deml <stefandeml@gmail.com>
2018-12-08 07:41:40 +0000

    use GH_TOKEN as env

--- scripts/publish_book.sh
+++ scripts/publish_book.sh
@@ -10,5 +10,5 @@ git config --global user.email "stefan.deml+zokratesbot@decentriq.ch"
 git clone https://github.com/Zokrates/zokrates.github.io.git
 git clone https://github.com/davisp/ghp-import.git
 cd zokrates.github.io
-../ghp-import/ghp_import.py -n -p -f -m "Documentation upload" -b "master" -r https://17cd8c4c351b617eba3847d4f2af39c997e9758b@github.com/Zokrates/zokrates.github.io.git ../book
+../ghp-import/ghp_import.py -n -p -f -m "Documentation upload" -b "master" -r https://zokratesbot:"$GH_TOKEN"@github.com/Zokrates/zokrates.github.io.git ../book
 echo "Published book"
