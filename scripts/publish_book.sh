86aa43081fe2a362e28d22ab1494bd492d218171
sdeml <stefandeml@gmail.com>
2018-12-17 10:08:37 +0100

    use cargo version number instead of git tag

--- scripts/publish_book.sh
+++ scripts/publish_book.sh
@@ -11,12 +11,8 @@ if [ "$CIRCLE_BRANCH" == "develop" ]; then
     git clone https://github.com/Zokrates/zokrates.github.io.git
     git clone https://github.com/davisp/ghp-import.git
     cd zokrates.github.io
-    { 
-        TAG=$(git describe --tags) && echo $TAG
-    } || {
-        echo "No tag found"
-    }
-    ../ghp-import/ghp_import.py -n -p -f -m "Documentation upload $TAG" -b "master" -r https://zokratesbot:"$GH_TOKEN"@github.com/Zokrates/zokrates.github.io.git ../book
+    TAG=$(cat ../zokrates_cli/Cargo.toml | grep '^version' | awk '{print $3}' | sed -e 's/"//g') && echo $TAG
+    ../ghp-import/ghp_import.py -n -p -f -m "Documentation upload. Version:  $TAG" -b "master" -r https://zokratesbot:"$GH_TOKEN"@github.com/Zokrates/zokrates.github.io.git ../book
     echo "Published book"
 fi
 
