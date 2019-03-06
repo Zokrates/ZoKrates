0c714a1051fd0d8ba07fe8101da237d4275443a2
schaeff <thibaut@schaeff.fr>
2019-03-06 13:24:52 +0100

    fix warning

--- zokrates_cli/src/bin.rs
+++ zokrates_cli/src/bin.rs
@@ -8,8 +8,6 @@ use bincode::{deserialize_from, serialize_into, Infinite};
 use clap::{App, AppSettings, Arg, SubCommand};
 use std::env;
 use std::fs::File;
-#[cfg(feature = "libsnark")]
-use std::io::BufRead;
 use std::io::{stdin, BufReader, BufWriter, Read, Write};
 use std::path::{Path, PathBuf};
 use std::string::String;
