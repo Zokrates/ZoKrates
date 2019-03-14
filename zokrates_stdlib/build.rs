688c3ca4e535a4198e7bb23013f16852044df76a
schaeff <thibaut@schaeff.fr>
2019-03-14 16:58:49 +0100

    add libsnark support, use glob for folders

--- zokrates_stdlib/build.rs
+++ zokrates_stdlib/build.rs
@@ -1,36 +1,37 @@
+use glob::glob;
 use std::env;
-use std::fs::read_dir;
-use std::fs::DirEntry;
 use std::fs::File;
 use std::io::Write;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
-// build script's entry point
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     let destination = Path::new(&out_dir).join("tests.rs");
     let mut test_file = File::create(&destination).unwrap();
 
-    // write test file header, put `use`, `const` etc there
-    //write_header(&mut test_file);
-
-    let test_data_directories = read_dir("./tests/bench/").unwrap();
-
-    for (index, directory) in test_data_directories.enumerate() {
-        write_test(&mut test_file, &directory.unwrap(), index);
+    for directory in glob("./tests/bench/**/*.json").unwrap() {
+        write_test(&mut test_file, &directory.unwrap());
     }
 }
 
-fn write_test(test_file: &mut File, directory: &DirEntry, count: usize) {
-    let directory = directory.path().canonicalize().unwrap();
-    let test_path = directory.display();
-    let test_name = format!("test_{}", count);
+fn write_test(test_file: &mut File, test_path: &PathBuf) {
+    let test_name = format!(
+        "test_{}",
+        test_path
+            .strip_prefix("tests/bench")
+            .unwrap()
+            .display()
+            .to_string()
+            .replace("/", "_")
+            .replace(".json", "")
+            .replace(".", "")
+    );
 
     write!(
         test_file,
         include_str!("./tests/test_template"),
         test_name = test_name,
-        test_path = test_path
+        test_path = test_path.display()
     )
     .unwrap();
 }
