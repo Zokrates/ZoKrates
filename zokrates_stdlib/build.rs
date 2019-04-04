ad5a9098633fa40f4544adc1dfa8ff756186a9b1
schaeff <thibaut@schaeff.fr>
2019-04-04 19:32:34 +0200

    copy stdlib to OUT_DIR

--- zokrates_stdlib/build.rs
+++ zokrates_stdlib/build.rs
@@ -1,10 +1,32 @@
-use glob::glob;
+use fs_extra::copy_items;
+use fs_extra::dir::CopyOptions;
 use std::env;
-use std::fs::File;
-use std::io::Write;
-use std::path::{Path, PathBuf};
 
 fn main() {
+    // export stdlib folder to OUT_DIR
+    export_stdlib();
+
+    // generate tests
+    #[cfg(test)]
+    {
+        write_tests();
+    }
+}
+
+fn export_stdlib() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let mut options = CopyOptions::new();
+    options.overwrite = true;
+    copy_items(&vec!["stdlib"], out_dir, &options).unwrap();
+}
+
+#[cfg(test)]
+fn write_tests() {
+    use glob::glob;
+    use std::fs::File;
+    use std::io::Write;
+    use std::path::{Path, PathBuf};
+
     let out_dir = env::var("OUT_DIR").unwrap();
     let destination = Path::new(&out_dir).join("tests.rs");
     let mut test_file = File::create(&destination).unwrap();
@@ -14,6 +36,7 @@ fn main() {
     }
 }
 
+#[cfg(test)]
 fn write_test(test_file: &mut File, test_path: &PathBuf) {
     let test_name = format!(
         "test_{}",
