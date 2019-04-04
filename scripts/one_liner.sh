f963a4755efb6869ce80daf1697039f9b7cea7fb
schaeff <thibaut@schaeff.fr>
2019-04-04 16:44:43 +0200

    remove rustc dep

--- scripts/one_liner.sh
+++ scripts/one_liner.sh
@@ -16,8 +16,200 @@ Options:
 EOF
 }
 
+get_bitness() {
+    need_cmd head
+    # Architecture detection without dependencies beyond coreutils.
+    # ELF files start out "\x7fELF", and the following byte is
+    #   0x01 for 32-bit and
+    #   0x02 for 64-bit.
+    # The printf builtin on some shells like dash only supports octal
+    # escape sequences, so we use those.
+    local _current_exe_head
+    _current_exe_head=$(head -c 5 /proc/self/exe )
+    if [ "$_current_exe_head" = "$(printf '\177ELF\001')" ]; then
+        echo 32
+    elif [ "$_current_exe_head" = "$(printf '\177ELF\002')" ]; then
+        echo 64
+    else
+        err "unknown platform bitness"
+    fi
+}
+
+get_endianness() {
+    local cputype=$1
+    local suffix_eb=$2
+    local suffix_el=$3
+
+    # detect endianness without od/hexdump, like get_bitness() does.
+    need_cmd head
+    need_cmd tail
+
+    local _current_exe_endianness
+    _current_exe_endianness="$(head -c 6 /proc/self/exe | tail -c 1)"
+    if [ "$_current_exe_endianness" = "$(printf '\001')" ]; then
+        echo "${cputype}${suffix_el}"
+    elif [ "$_current_exe_endianness" = "$(printf '\002')" ]; then
+        echo "${cputype}${suffix_eb}"
+    else
+        err "unknown platform endianness"
+    fi
+}
+
+get_architecture() {
+    local _ostype _cputype _bitness _arch
+    _ostype="$(uname -s)"
+    _cputype="$(uname -m)"
+
+    if [ "$_ostype" = Linux ]; then
+        if [ "$(uname -o)" = Android ]; then
+            _ostype=Android
+        fi
+    fi
+
+    if [ "$_ostype" = Darwin ] && [ "$_cputype" = i386 ]; then
+        # Darwin `uname -m` lies
+        if sysctl hw.optional.x86_64 | grep -q ': 1'; then
+            _cputype=x86_64
+        fi
+    fi
+
+    case "$_ostype" in
+
+        Android)
+            _ostype=linux-android
+            ;;
+
+        Linux)
+            _ostype=unknown-linux-gnu
+            _bitness=$(get_bitness)
+            ;;
+
+        FreeBSD)
+            _ostype=unknown-freebsd
+            ;;
+
+        NetBSD)
+            _ostype=unknown-netbsd
+            ;;
+
+        DragonFly)
+            _ostype=unknown-dragonfly
+            ;;
+
+        Darwin)
+            _ostype=apple-darwin
+            ;;
+
+        MINGW* | MSYS* | CYGWIN*)
+            _ostype=pc-windows-gnu
+            ;;
+
+        *)
+            err "unrecognized OS type: $_ostype"
+            ;;
+
+    esac
+
+    case "$_cputype" in
+
+        i386 | i486 | i686 | i786 | x86)
+            _cputype=i686
+            ;;
+
+        xscale | arm)
+            _cputype=arm
+            if [ "$_ostype" = "linux-android" ]; then
+                _ostype=linux-androideabi
+            fi
+            ;;
+
+        armv6l)
+            _cputype=arm
+            if [ "$_ostype" = "linux-android" ]; then
+                _ostype=linux-androideabi
+            else
+                _ostype="${_ostype}eabihf"
+            fi
+            ;;
+
+        armv7l | armv8l)
+            _cputype=armv7
+            if [ "$_ostype" = "linux-android" ]; then
+                _ostype=linux-androideabi
+            else
+                _ostype="${_ostype}eabihf"
+            fi
+            ;;
+
+        aarch64)
+            _cputype=aarch64
+            ;;
+
+        x86_64 | x86-64 | x64 | amd64)
+            _cputype=x86_64
+            ;;
+
+        mips)
+            _cputype=$(get_endianness mips '' el)
+            ;;
+
+        mips64)
+            if [ "$_bitness" -eq 64 ]; then
+                # only n64 ABI is supported for now
+                _ostype="${_ostype}abi64"
+                _cputype=$(get_endianness mips64 '' el)
+            fi
+            ;;
+
+        ppc)
+            _cputype=powerpc
+            ;;
+
+        ppc64)
+            _cputype=powerpc64
+            ;;
+
+        ppc64le)
+            _cputype=powerpc64le
+            ;;
+
+        *)
+            err "unknown CPU type: $_cputype"
+
+    esac
+
+    # Detect 64-bit linux with 32-bit userland
+    if [ "${_ostype}" = unknown-linux-gnu ] && [ "${_bitness}" -eq 32 ]; then
+        case $_cputype in
+            x86_64)
+                _cputype=i686
+                ;;
+            mips64)
+                _cputype=$(get_endianness mips '' el)
+                ;;
+            powerpc64)
+                _cputype=powerpc
+                ;;
+        esac
+    fi
+
+    # Detect armv7 but without the CPU features Rust needs in that build,
+    # and fall back to arm.
+    # See https://github.com/rust-lang/rustup.rs/issues/587.
+    if [ "$_ostype" = "unknown-linux-gnueabihf" ] && [ "$_cputype" = armv7 ]; then
+        if ensure grep '^Features' /proc/cpuinfo | grep -q -v neon; then
+            # At least one processor does not have NEON.
+            _cputype=arm
+        fi
+    fi
+
+    _arch="${_cputype}-${_ostype}"
+
+    RETVAL="$_arch"
+}
+
 say() {
-    echo "install.sh: $1"
+    echo "ZoKrates: $1"
 }
 
 say_err() {
@@ -38,6 +230,16 @@ need() {
         err "need $1 (command not found)"
     fi
 }
+
+## MAIN ##
+
+## curl installed? 
+which curl &> /dev/null 
+if [[ $? -ne 0 ]] ; then
+    echo '"curl" binary not found, please install and retry'
+    exit 1
+fi
+##
 
 force=false
 while test $# -gt 0; do
@@ -66,7 +268,6 @@ need install
 need mkdir
 need mktemp
 need tar
-need rustc
 
 # Optional dependencies
 if [ -z $tag ]; then
@@ -77,13 +278,10 @@ fi
 git="schaeff/zokrates"
 
 url="https://github.com/$git"
-say_err "GitHub repository: $url"
 
 if [ -z $crate ]; then
     crate=$(echo $git | cut -d'/' -f2)
 fi
-
-say_err "Crate: $crate"
 
 url="$url/releases"
 
@@ -94,11 +292,17 @@ else
     say_err "Tag: $tag"
 fi
 
-if [ -z $target ]; then
-    target=$(rustc -Vv | grep host | cut -d' ' -f2)
-fi
+get_architecture || return 1
+arch="$RETVAL"
 
-say_err "Target: $target"
+ext="tar.gz"
+case "$_arch" in
+    *windows*)
+        ext=".exe"
+        ;;
+esac
+
+say_err "Detected architecture: $arch"
 
 if [ -z $dest ]; then
     dest="$HOME/.cargo/bin"
@@ -106,9 +310,9 @@ fi
 
 say_err "Installing to: $dest"
 
-url="$url/download/$tag/$crate-$tag-$target.tar.gz"
+url="$url/download/$tag/$crate-$tag-$arch.$ext"
 
-say_err "Url: $url"
+say_err "Fetching from: $url"
 
 td=$(mktemp -d || mktemp -d -t tmp)
 curl -sLf --show-error $url | tar -C $td -xz
