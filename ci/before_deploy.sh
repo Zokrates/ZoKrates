b93bf6f1efd009a680ba4821eb8c250b573b437d
Thibaut Schaeffer <schaeffer.thibaut@gmail.com>
2019-04-11 23:20:44 +0200

    Create binaries with libsnark disabled on new tags (#319)
    * add appveyor, travis, remove libsnark as default, add push to gh
    
    * fix appveyor
    
    * change appveyor to nightly
    
    * use cargo for build job
    
    * change to build everywhere
    
    * update token, reduce targets
    
    * add more targets
    
    * add on liner script
    
    * remove rustc dep
    
    * add need_cmd
    
    * add check cmd
    
    * simple brackets
    
    * use check_cmd for curl
    
    * copy stdlib to OUT_DIR
    
    * ship stdlib in archive
    
    * install stdlib, give PATH hints after build script
    
    * clean
    
    * comment
    
    * scripts: ensure one-liner install script works with FreeBSD
    
    * remove appveyor for now
    
    * clean, remove windows, remove appveyor, remove travis test
    
    * put back cross install
    
    * fix stdlib test generation
    
    * disable tests in travis
    
    * restore test script to prevent tests to run..
    
    * change scripts to keep same behaviour now that libsnark is disabled by default
    
    * update docs with installer
    
    * change docker to dockerhub
    
    * change repo

---
+++ ci/before_deploy.sh
@@ -0,0 +1,35 @@
+# This script takes care of building your crate and packaging it for release
+
+set -ex
+
+main() {
+    local src=$(pwd) \
+          stage=
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            ;;
+    esac
+
+    test -f Cargo.lock || cargo generate-lockfile
+
+    cross build --bin zokrates --target $TARGET --release
+
+    # Package artifacts
+    # Binary
+    cp target/$TARGET/release/zokrates $stage/
+    # Standard library
+    cp -r zokrates_stdlib/stdlib $stage
+
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    cd $src
+
+    rm -rf $stage
+}
+
+main
--- zokrates_stdlib/build.rs
+++ zokrates_stdlib/build.rs
@@ -1,10 +1,28 @@
-use glob::glob;
+use fs_extra::copy_items;
+use fs_extra::dir::CopyOptions;
 use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::path::{Path, PathBuf};
 
 fn main() {
+    // export stdlib folder to OUT_DIR
+    export_stdlib();
+
+    // generate tests
+    write_tests();
+}
+
+fn export_stdlib() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let mut options = CopyOptions::new();
+    options.overwrite = true;
+    copy_items(&vec!["stdlib"], out_dir, &options).unwrap();
+}
+
+fn write_tests() {
+    use glob::glob;
+
     let out_dir = env::var("OUT_DIR").unwrap();
     let destination = Path::new(&out_dir).join("tests.rs");
     let mut test_file = File::create(&destination).unwrap();
