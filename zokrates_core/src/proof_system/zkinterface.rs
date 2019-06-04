5607bb7c170dc343d97e01c43a14db204370ad4c
ron <ron@qed-it.com>
2019-06-04 11:48:38 +0300

    fix: adjust zk-interface to the current ProofSystem interface

--- zokrates_core/src/proof_system/zkinterface.rs
+++ zokrates_core/src/proof_system/zkinterface.rs
@@ -1,6 +1,8 @@
 extern crate core;
 extern crate libc;
 
+use ir::{self, Statement};
+use std::collections::HashMap;
 use flat_absy::flat_variable::FlatVariable;
 use proof_system::ProofSystem;
 use std::fs::File;
@@ -22,6 +24,7 @@ use zkinterface::{
         WitnessArgs,
     },
 };
+
 use zokrates_field::field::{Field, FieldPrime};
 
 pub struct ZkInterface {}
@@ -31,22 +34,13 @@ impl ZkInterface {
         ZkInterface {}
     }
 }
-
 impl ProofSystem for ZkInterface {
-    fn setup(
-        &self,
-        variables: Vec<FlatVariable>,
-        a: Vec<Vec<(usize, FieldPrime)>>,
-        b: Vec<Vec<(usize, FieldPrime)>>,
-        c: Vec<Vec<(usize, FieldPrime)>>,
-        num_public_inputs: usize,
-        pk_path: &str,
-        _vk_path: &str,
-    ) -> bool {
-        let first_local_id = 1 + num_public_inputs as u64;
-        let free_variable_id = variables.len() as u64;
-
-        // Write R1CSConstraints message.
+    fn setup(&self, program: ir::Prog<FieldPrime>, pk_path: &str, vk_path: &str) {
+    // transform to R1CS
+    let (variables, public_variables_count, a, b, c) = r1cs_program(program);
+    let first_local_id = 1 + public_variables_count as u64;
+    let free_variable_id = variables.len() as u64;
+    // Write R1CSConstraints message.
         write_r1cs(&a, &b, &c, pk_path);
 
         // Write Return message including free_variable_id.
@@ -57,29 +51,33 @@ impl ProofSystem for ZkInterface {
             true,
             &format!("circuit_{}", pk_path));
 
-        true
-    }
+}
 
     fn generate_proof(
         &self,
-        _pk_path: &str,
+        program: ir::Prog<FieldPrime>,
+        witness: ir::Witness<FieldPrime>,
+        pk_path: &str,
         proof_path: &str,
-        public_inputs: Vec<FieldPrime>,
-        local_values: Vec<FieldPrime>,
     ) -> bool {
-        let first_local_id = public_inputs.len() as u64;
-        let free_variable_id = first_local_id + local_values.len() as u64;
+        let (
+            public_inputs_arr,
+            private_inputs_arr,
+        ) = prepare_generate_proof(program, witness);
 
-        println!("{:?}", public_inputs);
+        let first_local_id = public_inputs_arr.len() as u64;
+        let free_variable_id = first_local_id + private_inputs_arr.len() as u64;
+
+//        println!("{:?}", program);
 
         // Write assignment to local variables.
-        write_assignment(first_local_id as u64, &local_values, proof_path);
+        write_assignment(first_local_id as u64, &private_inputs_arr, proof_path);
 
         // Write Return message including output values.
         write_ciruit(
             first_local_id,
             free_variable_id,
-            Some(&public_inputs),
+            Some(&public_inputs_arr),
             false,
             &format!("circuit_{}", proof_path));
 
@@ -155,7 +153,6 @@ fn convert_linear_combination<'a>(builder: &mut FlatBufferBuilder<'a>, item: &Ve
     })
 }
 
-
 fn write_assignment(
     first_local_id: u64,
     local_values: &[FieldPrime],
@@ -226,4 +223,124 @@ fn write_ciruit(
     println!("Writing {}", to_path);
     let mut file = File::create(to_path).unwrap();
     gadget_return.write(&mut file).unwrap();
+}
+
+
+pub fn prepare_generate_proof<T: Field>(
+    program: ir::Prog<T>,
+    witness: ir::Witness<T>,
+) -> (Vec<T>,Vec<T>) {
+    // recover variable order from the program
+    let (variables, public_variables_count, _, _, _) = r1cs_program(program);
+
+    let witness: Vec<T> = variables.iter().map(|x| witness.0[x].clone()).collect();
+
+    // split witness into public and private inputs at offset
+    let mut public_inputs: Vec<T> = witness.clone();
+    let private_inputs: Vec<T> = public_inputs.split_off(public_variables_count);
+
+    (
+        public_inputs,
+        private_inputs,
+    )
+}
+
+pub fn provide_variable_idx(
+    variables: &mut HashMap<FlatVariable, usize>,
+    var: &FlatVariable,
+) -> usize {
+    let index = variables.len();
+    *variables.entry(*var).or_insert(index)
+}
+
+pub fn r1cs_program<T: Field>(
+    prog: ir::Prog<T>,
+) -> (
+    Vec<FlatVariable>,
+    usize,
+    Vec<Vec<(usize, T)>>,
+    Vec<Vec<(usize, T)>>,
+    Vec<Vec<(usize, T)>>,
+) {
+    let mut variables: HashMap<FlatVariable, usize> = HashMap::new();
+    provide_variable_idx(&mut variables, &FlatVariable::one());
+
+    for x in prog
+        .main
+        .arguments
+        .iter()
+        .enumerate()
+        .filter(|(index, _)| !prog.private[*index])
+        {
+            provide_variable_idx(&mut variables, &x.1);
+        }
+
+    //Only the main function is relevant in this step, since all calls to other functions were resolved during flattening
+    let main = prog.main;
+
+    //~out are added after main's arguments as we want variables (columns)
+    //in the r1cs to be aligned like "public inputs | private inputs"
+    let main_return_count = main.returns.len();
+
+    for i in 0..main_return_count {
+        provide_variable_idx(&mut variables, &FlatVariable::public(i));
+    }
+
+    // position where private part of witness starts
+    let private_inputs_offset = variables.len();
+
+    // first pass through statements to populate `variables`
+    for (quad, lin) in main.statements.iter().filter_map(|s| match s {
+        Statement::Constraint(quad, lin) => Some((quad, lin)),
+        Statement::Directive(..) => None,
+    }) {
+        for (k, _) in &quad.left.0 {
+            provide_variable_idx(&mut variables, &k);
+        }
+        for (k, _) in &quad.right.0 {
+            provide_variable_idx(&mut variables, &k);
+        }
+        for (k, _) in &lin.0 {
+            provide_variable_idx(&mut variables, &k);
+        }
+    }
+
+    let mut a = vec![];
+    let mut b = vec![];
+    let mut c = vec![];
+
+    // second pass to convert program to raw sparse vectors
+    for (quad, lin) in main.statements.into_iter().filter_map(|s| match s {
+        Statement::Constraint(quad, lin) => Some((quad, lin)),
+        Statement::Directive(..) => None,
+    }) {
+        a.push(
+            quad.left
+                .0
+                .into_iter()
+                .map(|(k, v)| (variables.get(&k).unwrap().clone(), v))
+                .collect(),
+        );
+        b.push(
+            quad.right
+                .0
+                .into_iter()
+                .map(|(k, v)| (variables.get(&k).unwrap().clone(), v))
+                .collect(),
+        );
+        c.push(
+            lin.0
+                .into_iter()
+                .map(|(k, v)| (variables.get(&k).unwrap().clone(), v))
+                .collect(),
+        );
+    }
+
+    // Convert map back into list ordered by index
+    let mut variables_list = vec![FlatVariable::new(0); variables.len()];
+    for (k, v) in variables.drain() {
+        assert_eq!(variables_list[v], FlatVariable::new(0));
+        std::mem::replace(&mut variables_list[v], k);
+    }
+    (variables_list, private_inputs_offset, a, b, c)
 }
