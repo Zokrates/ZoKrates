0eeb82e2b86e5d2711eb7cdd5afe83edafa882d3
schaeff <thibaut@schaeff.fr>
2019-06-06 20:14:37 +0200

    test all crates no matter what

--- full_test.sh
+++ full_test.sh
@@ -3,8 +3,8 @@
 # Exit if any subcommand fails
 set -e
 
+cargo test --release -- --ignored
+
 if [ -n "$WITH_LIBSNARK" ]; then
 	cargo -Z package-features test --release --package zokrates_cli --features="libsnark" -- --ignored
-else
-	cargo test --release -- --ignored
 fi
