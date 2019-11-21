ec1d68a8cba11054eb522ffcb328611a0c5b0e4d
dark64 <darem966@gmail.com>
2019-11-21 17:54:07 +0100

    cargo fmt

--- zokrates_js/src/lib.rs
+++ zokrates_js/src/lib.rs
@@ -1,9 +1,9 @@
 use wasm_bindgen::prelude::*;
-use zokrates_core::compile::{ compile as compile_core, CompileErrors };
+use zokrates_abi::{parse_strict, Encode, Inputs};
+use zokrates_core::compile::{compile as compile_core, CompileErrors};
 use zokrates_core::ir;
-use zokrates_field::field::{ FieldPrime };
 use zokrates_core::proof_system::{self, ProofSystem};
-use zokrates_abi::{ parse_strict, Inputs, Encode };
+use zokrates_field::field::FieldPrime;
 
 extern crate serde_derive;
 
@@ -43,14 +43,17 @@ pub fn compile(source: JsValue, location: JsValue) -> Result<JsValue, JsValue> {
         let result = resolve(l, p.clone());
         match result {
             Some(res) => Ok((res.source, res.location)),
-            None => Err(zokrates_core::imports::Error::new(String::from(format!("Unable to resolve {}", p))))
+            None => Err(zokrates_core::imports::Error::new(String::from(format!(
+                "Unable to resolve {}",
+                p
+            )))),
         }
     };
 
     let program_flattened: Result<ir::Prog<FieldPrime>, CompileErrors> = compile_core(
-        source.as_string().unwrap(), 
-        location.as_string().unwrap(), 
-        Some(resolve_closure)
+        source.as_string().unwrap(),
+        location.as_string().unwrap(),
+        Some(resolve_closure),
     );
 
     match program_flattened {
@@ -58,7 +61,7 @@ pub fn compile(source: JsValue, location: JsValue) -> Result<JsValue, JsValue> {
             let data: Vec<u8> = bincode::serialize(&p).unwrap();
             Ok(JsValue::from_serde(&data).unwrap())
         }
-        Err(ce) => Err(JsValue::from_str(&format!("{}", ce)))
+        Err(ce) => Err(JsValue::from_str(&format!("{}", ce))),
     }
 }
 
@@ -70,35 +73,32 @@ pub fn compute_witness(program: JsValue, args: JsValue) -> Result<JsValue, JsVal
     let signature = program_flattened.signature.clone();
 
     let arguments = parse_strict(&input, signature.inputs)
-                        .map(|parsed| Inputs::Abi(parsed))
-                        .map_err(|why| why.to_string());
+        .map(|parsed| Inputs::Abi(parsed))
+        .map_err(|why| why.to_string());
 
-    match arguments {   
+    match arguments {
         Ok(inputs) => {
             let witness = program_flattened.execute(&inputs.encode());
             match witness {
                 Ok(witness) => Ok(JsValue::from_str(&format!("{}", witness))),
-                Err(error) => Err(JsValue::from_str(&format!("Execution failed: {}", error)))
+                Err(error) => Err(JsValue::from_str(&format!("Execution failed: {}", error))),
             }
-        },
-        Err(err) => Err(JsValue::from_str(&format!("{}", err)))
+        }
+        Err(err) => Err(JsValue::from_str(&format!("{}", err))),
     }
-
 }
 
 #[wasm_bindgen]
 pub fn setup(program: JsValue) -> JsValue {
     let program_flattened = deserialize_program(&program);
-    let proof = proof_system::G16{}.setup(program_flattened);
+    let proof = proof_system::G16 {}.setup(program_flattened);
     JsValue::from_serde(&proof).unwrap()
 }
 
 #[wasm_bindgen]
 pub fn export_solidity_verifier(vk: JsValue, is_abiv2: JsValue) -> JsValue {
-    let verifier = proof_system::G16{}.export_solidity_verifier(
-        vk.as_string().unwrap(), 
-        is_abiv2.as_bool().unwrap()
-    );
+    let verifier = proof_system::G16 {}
+        .export_solidity_verifier(vk.as_string().unwrap(), is_abiv2.as_bool().unwrap());
     JsValue::from_str(verifier.as_str())
 }
 
@@ -109,13 +109,13 @@ pub fn generate_proof(program: JsValue, witness: JsValue, pk: JsValue) -> JsValu
     let witness_out: ir::Witness<FieldPrime> = ir::Witness::read(str_witness.as_bytes()).unwrap();
     let proving_key: Vec<u8> = pk.into_serde().unwrap();
 
-    let proof: String = proof_system::G16{}.generate_proof(program_flattened, witness_out, proving_key);
+    let proof: String =
+        proof_system::G16 {}.generate_proof(program_flattened, witness_out, proving_key);
     JsValue::from_str(proof.as_str())
 }
 
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
-
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
