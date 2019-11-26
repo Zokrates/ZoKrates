1c706ca53cc46b08df4533657128172486f9d386
dark64 <darem966@gmail.com>
2019-11-26 18:20:03 +0100

    Add publish.sh

---
+++ zokrates_js/publish.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+NPM_VERSION=$(npm view zokrates-js dist-tags.latest)
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+CARGO_VERSION=$(cat Cargo.toml \
+  | grep '^version' \
+  | awk '{print $3}' \
+  | sed -e 's/"//g')
+
+if [ $NPM_VERSION = $PACKAGE_VERSION ]; then
+    echo "Latest npm version is equal to current package version. Up the version to publish to npm."
+    exit 1
+fi
+
+if [ $PACKAGE_VERSION != $CARGO_VERSION ]; then
+    echo "Cargo crate version must be equal to npm package version ($CARGO_VERSION -> $PACKAGE_VERSION)"
+    exit 1
+fi
+
+# npm
+npm run build
+npm run test
+
+# publish package
+npm set //registry.npmjs.org/:_authToken=${NPM_TOKEN}
+npm publish
