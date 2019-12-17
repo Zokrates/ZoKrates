f21505a395fcf9e9ac454691207ba897b83b91d9
dark64 <darem966@gmail.com>
2019-12-17 17:38:51 +0100

    refactor js

--- zokrates_js/js/index.d.ts
+++ zokrates_js/js/index.d.ts
@@ -1,17 +1,23 @@
-export interface ResolverResult {
-  source: string,
-  location: string
-}
+declare module 'zokrates-js' {
 
-export interface SetupKeypair {
-  vk: string,
-  pk: Uint8Array,
+  export interface ResolverResult {
+    source: string,
+    location: string
+  }
+  
+  export interface SetupKeypair {
+    vk: string,
+    pk: Uint8Array,
+  }
+  
+  export interface ZoKratesProvider {
+    compile(source: string, location: string): Uint8Array;
+    setup(program: Uint8Array): SetupKeypair;
+    computeWitness(program: Uint8Array, args: string[]): string;
+    exportSolidityVerifier(verifyingKey: string, isAbiv2: boolean): string
+    generateProof(program: Uint8Array, witness: string, provingKey: Uint8Array): string;
+  }
+  
+  export type ResolveCallback = (location: string, path: string) => ResolverResult;
+  export function initialize(callback: ResolveCallback): Promise<ZoKratesProvider>;
 }
-
-export function initialize(callback: (location: string, path: string) => ResolverResult): Promise<void>;
-export function compile(source: string, location: string): Uint8Array;
-export function getStdLib(): object;
-export function computeWitness(program: Uint8Array, args: string[]): string;
-export function setup(program: Uint8Array): SetupKeypair;
-export function exportSolidityVerifier(verifyingKey: string, isAbiv2: boolean): string;
-export function generateProof(program: Uint8Array, witness: string, provingKey: Uint8Array): string;
--- zokrates_js/js/index.js
+++ zokrates_js/js/index.js
@@ -1,68 +1,63 @@
-import { getAbsolutePath, appendExtension } from './utils';
+import { appendExtension, getAbsolutePath } from './utils';
 import stdlib from '../stdlib.json';
 
-const __extension = '.zok';
-const __reserved  = ['ecc/', 'signature/', 'hashes/', 'utils/'];
-var   __state = {};
+const initialize = async (callback) => {
 
-export async function initialize(callback) {
-  let zokrates = await import('../pkg/index.js');
-  __state.zokrates = zokrates;
+  const EXTENSION_ZOK  = '.zok';
+  const RESERVED_PATHS = [
+    'ecc/',
+    'signature/',
+    'hashes/', 
+    'utils/'
+  ];
 
-  window.resolve = function (location, path) {
-    return __resolve(location, path, callback);
+  // load web assembly module
+  const zokrates = await import('../pkg/index.js');
+
+  // register module resolving callback to provided namespace
+  globalThis.resolve = (location, path) => resolveModule(location, path, callback);
+
+  const resolveModule = (location, path, callback) => {
+    let result;
+    if (isReserved(location) || isReserved(path)) {
+      result = resolveFromStandardLibrary(location, path);
+    } else {
+      result = callback(location, path);
+    } 
+    if (!result) return null;
+    return zokrates.ResolverResult.new(result.source, result.location);
   }
-} 
 
-function __isReservedPath(path) {
-  return __reserved.some(function (p) { return path.startsWith(p) || path.startsWith(p) })
-}
+  const isReserved = (path) => RESERVED_PATHS.some(p => path.startsWith(p));
 
-function __resolve(location, path, callback) {
-  let result;
-  if(__isReservedPath(location)) {
-    result = resolveFromStdlib(location, path);
-  } else {
-    result = callback(location, path);
-  } 
-  if (!result) return null;
-  return __state.zokrates.ResolverResult.new(result.source, result.location);
-}
-
-export function resolveFromStdlib(location, path) {
-  let key = appendExtension(getAbsolutePath(location, path), __extension);
-  let source = stdlib[key];
-  return source ? { source, location: key } : null;
-}
-
-export function getStdLib() {
-  return stdlib;
-}
-
-export function compile(source, location) {
-  if (typeof __state.zokrates === 'undefined') {
-    throw new Error("You must call initialize() before calling this method")
+  const resolveFromStandardLibrary = (location, path) => {
+    let key = appendExtension(getAbsolutePath(location, path), EXTENSION_ZOK);
+    let source = stdlib[key];
+    return source ? { source, location: key } : null;
   }
-  return Uint8Array.from(__state.zokrates.compile(source, location));
+
+  return {
+    compile: (source, location) => {
+      return Uint8Array.from(zokrates.compile(source, location));
+    },
+    setup: (program) => {
+      let result = zokrates.setup(Array.from(program));
+      return { 
+        vk: result.vk, 
+        pk: Uint8Array.from(result.pk) 
+      };
+    },
+    computeWitness: (program, args) => {
+      const input = JSON.stringify(Array.from(args));
+      return zokrates.compute_witness(Array.from(program), input);
+    },
+    exportSolidityVerifier: (verifyingKey, isAbiv2) => {
+      return zokrates.export_solidity_verifier(verifyingKey, isAbiv2);
+    },
+    generateProof: (program, witness, provingKey) => {
+      return zokrates.generate_proof(Array.from(program), witness, Array.from(provingKey));
+    }
+  }
 }
 
-export function computeWitness(program, args) {
-  const input = JSON.stringify(Array.from(args));
-  return __state.zokrates.compute_witness(Array.from(program), input);
-}
-
-export function setup(program) {
-  let result = __state.zokrates.setup(Array.from(program));
-  return { 
-    vk: result.vk, 
-    pk: Uint8Array.from(result.pk) 
-  };
-}
-
-export function exportSolidityVerifier(verifyingKey, isAbiv2) {
-  return __state.zokrates.export_solidity_verifier(verifyingKey, isAbiv2);
-}
-
-export function generateProof(program, witness, provingKey) {
-  return __state.zokrates.generate_proof(Array.from(program), witness, Array.from(provingKey));
-}
+export { initialize };
