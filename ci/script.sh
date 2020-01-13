ebdca59d20f5afb60faf04e45e582754385315a0
schaeff <thibaut@schaeff.fr>
2020-01-13 14:19:59 +0100

    make libsnark install cross platform, run before travis build

--- ci/script.sh
+++ ci/script.sh
@@ -4,6 +4,7 @@ set -ex
 
 # This is the test phase. We will only build as tests happened before.
 main() {
+	./scripts/install_libsnark_prerequisites.sh
     cross build --target $TARGET
     cross build --target $TARGET --release
 }
--- scripts/install_libsnark_prerequisites.sh
+++ scripts/install_libsnark_prerequisites.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y --no-install-recommends \
-    build-essential \
-    cmake \
-    libboost-dev \
-    libboost-program-options-dev \
-    libgmp3-dev \
-    libprocps-dev \
-    libssl-dev \
-    pkg-config \
-    python-markdown \
-    && rm -rf /var/lib/apt/lists/* \
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    apt-get update && apt-get install -y --no-install-recommends \
+        build-essential \
+        cmake \
+        libboost-dev \
+        libboost-program-options-dev \
+        libgmp3-dev \
+        libprocps-dev \
+        libssl-dev \
+        pkg-config \
+        python-markdown \
+        && rm -rf /var/lib/apt/lists/*
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install boost
+    brew install cmake
+    brew install openssl
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+    echo "Libsnark not supported"
+elif [[ "$OSTYPE" == "msys" ]]; then
+    echo "Libsnark not supported"
+elif [[ "$OSTYPE" == "win32" ]]; then
+    echo "Libsnark not supported"
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+    echo "Libsnark not supported"
+else
+    echo "Libsnark not supported"
+fi
