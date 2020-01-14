76e03581d13be19af344f1e4ee2808f039f152df
schaeff <thibaut@schaeff.fr>
2020-01-14 10:37:50 +0100

    check if brew packages are installed

--- scripts/install_libsnark_prerequisites.sh
+++ scripts/install_libsnark_prerequisites.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    apt-get update && apt-get install -y --no-install-recommends \
-        build-essential \
-        cmake \
-        libboost-dev \
-        libboost-program-options-dev \
-        libgmp3-dev \
-        libprocps-dev \
-        libssl-dev \
-        pkg-config \
-        python-markdown \
-        && rm -rf /var/lib/apt/lists/*
+    # apt-get update && apt-get install -y --no-install-recommends \
+    #     build-essential \
+    #     cmake \
+    #     libboost-dev \
+    #     libboost-program-options-dev \
+    #     libgmp3-dev \
+    #     libprocps-dev \
+    #     libssl-dev \
+    #     pkg-config \
+    #     python-markdown \
+    #     && rm -rf /var/lib/apt/lists/*
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install boost
-    brew install cmake
-    brew install openssl
+    # brew install boost
+    # brew install cmake
+    # brew install openssl
+
+    brew info boost
+    brew info cmake
+    brew info openssl
 elif [[ "$OSTYPE" == "cygwin" ]]; then
     echo "Libsnark not supported"
 elif [[ "$OSTYPE" == "msys" ]]; then
