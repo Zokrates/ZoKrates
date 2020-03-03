9feb1bccc964d4c997a33377608fb65f31ce5705
schaeff <thibaut@schaeff.fr>
2020-03-03 10:45:11 +0100

    remove unused file

--- zokrates_core/src/static_analysis/memoize.rs
+++
@@ -1,7 +0,0 @@
-use zokrates_field::field::Field;
-use std::collections::HashSet;
-use typed_absy::identifier::CallIdentifier;
-
-pub struct Memoizer<'ast, T: Field> {
-	identifiers: HashSet<CallIdentifier<'ast, T>>
-}
