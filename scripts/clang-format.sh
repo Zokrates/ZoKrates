2af231bf3b911ab664e395a0fb7a5a7437171125
dark64 <darem966@gmail.com>
2020-04-07 16:02:18 +0200

    fix clang-format script

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
+# Usage: ./clang-format.sh zokrates_core/lib
 
 dir=$1
-ret=0
 
 for file in $dir/*.cpp $dir/*.hpp; do
   clang-format -i -style=WebKit -verbose $file
-  out=$(git diff --exit-code $file)
-
-  if [ $? -ne 0 ]; then
-    ret=1
-    echo "ERROR: clang-format diff in: $file"
-    echo "$out"
-  fi
 done
-
-exit $ret
