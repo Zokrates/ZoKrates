7b13fb2233db88120dc4d105b3da2e384c3bfca5
schaeff <thibaut@schaeff.fr>
2020-06-04 12:34:13 +0200

    update example to that of issue, bump version numbers

--- zokrates_core/tests/out_of_range.rs
+++ zokrates_core/tests/out_of_range.rs
@@ -11,14 +11,15 @@ use zokrates_field::field::FieldPrime;
 #[test]
 fn out_of_range() {
     let source = r#"
-		def main(field a) -> ():
-			true == a < 1
-			return
+		def main(private field a) -> (field):
+	        field x = if a < 5555 then 3333 else 4444 fi
+	        x == 3333
+			return 1
 	"#
     .to_string();
 
-    // let's try to prove that "2 < 1" is true by exploiting
-    // the fact that `2*2 - 2*1` has two distinct bit decompositions
+    // let's try to prove that "10000 < 5555" is true by exploiting
+    // the fact that `2*10000 - 2*5555` has two distinct bit decompositions
     // we chose the one which is out of range, ie the sum check features an overflow
 
     let res: CompilationArtifacts<FieldPrime> =
@@ -27,6 +28,6 @@ fn out_of_range() {
     let interpreter = Interpreter::try_out_of_range();
 
     assert!(interpreter
-        .execute(&res.prog(), &vec![FieldPrime::from(2)])
+        .execute(&res.prog(), &vec![FieldPrime::from(10000)])
         .is_err());
 }
