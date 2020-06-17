5b9ea6eafe6ca4e767eae1d84249cb305384554b
Thibaut Schaeffer <thibaut@schaeff.fr>
2020-06-17 15:07:48 +0200

    Update zokrates_core/src/static_analysis/propagation.rs

--- zokrates_core/src/static_analysis/propagation.rs
+++ zokrates_core/src/static_analysis/propagation.rs
@@ -293,7 +293,7 @@ impl<'ast, T: Field> Folder<'ast, T> for Propagator<'ast, T> {
                                         }
                                     }
                                     "_SHA256_ROUND" => None,
-                                    k => None,
+                                    _ => None,
                                 };
 
                                 match r {
