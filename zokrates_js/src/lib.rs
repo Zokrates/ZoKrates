aeafaacfbd79ff80f5230bde44e147142c0e5b7a
dark64 <darem966@gmail.com>
2020-07-06 15:04:16 +0200

    fix zokrates_js

--- zokrates_js/src/lib.rs
+++ zokrates_js/src/lib.rs
@@ -10,8 +10,11 @@ use zokrates_core::compile::{
 };
 use zokrates_core::imports::Error;
 use zokrates_core::ir;
-use zokrates_core::proof_system::bellman::groth16::G16;
-use zokrates_core::proof_system::{ProofSystem, SolidityAbi};
+use zokrates_core::proof_system::bellman::Bellman;
+use zokrates_core::proof_system::schemes::groth16::G16;
+use zokrates_core::proof_system::schemes::Scheme;
+use zokrates_core::proof_system::solidity::SolidityAbi;
+use zokrates_core::proof_system::Backend;
 use zokrates_core::typed_absy::abi::Abi;
 use zokrates_core::typed_absy::types::Signature;
 use zokrates_field::Bn128Field;
@@ -163,7 +166,7 @@ pub fn compute_witness(artifacts: JsValue, args: JsValue) -> Result<JsValue, JsV
 pub fn setup(program: JsValue) -> Result<JsValue, JsValue> {
     let input: Vec<u8> = program.into_serde().unwrap();
     let program_flattened = deserialize_program(&input)?;
-    let keypair = G16::setup(program_flattened);
+    let keypair = Bellman::setup(program_flattened);
     Ok(JsValue::from_serde(&keypair).unwrap())
 }
 
@@ -172,7 +175,7 @@ pub fn export_solidity_verifier(vk: JsValue, abi_version: JsValue) -> Result<JsV
     let abi_version = SolidityAbi::from(abi_version.as_string().unwrap().as_str())
         .map_err(|err| JsValue::from_str(err))?;
 
-    let verifier = <G16 as ProofSystem<Bn128Field>>::export_solidity_verifier(
+    let verifier = <G16 as Scheme<Bn128Field>>::export_solidity_verifier(
         vk.into_serde().unwrap(),
         abi_version,
     );
@@ -190,7 +193,7 @@ pub fn generate_proof(program: JsValue, witness: JsValue, pk: JsValue) -> Result
         .map_err(|err| JsValue::from_str(&format!("Could not read witness: {}", err)))?;
 
     let proving_key: Vec<u8> = pk.into_serde().unwrap();
-    let proof = G16::generate_proof(program_flattened, ir_witness, proving_key);
+    let proof = Bellman::generate_proof(program_flattened, ir_witness, proving_key);
 
     Ok(JsValue::from_serde(&proof).unwrap())
 }
