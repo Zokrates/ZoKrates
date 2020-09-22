d7c45e5bd0324cab8ed73a0408ec49f0e38a3a58
dark64 <darem966@gmail.com>
2020-09-22 11:07:24 +0200

    fix integration test

--- zokrates_cli/tests/contract/test.js
+++ zokrates_cli/tests/contract/test.js
@@ -28,8 +28,8 @@ let jsonContractSource = JSON.stringify({
 let jsonInterface = JSON.parse(solc.compile(jsonContractSource));
 (async () => {
     const accounts = await web3.eth.getAccounts();
-    let abi = jsonInterface.contracts[contractPath]["Verifier"].abi
-    let bytecode = jsonInterface.contracts[contractPath]["Verifier"].evm.bytecode
+    let abi = jsonInterface.contracts[contractPath]["Verifier"].abi;
+    let bytecode = jsonInterface.contracts[contractPath]["Verifier"].evm.bytecode;
 
     //There is a solc issue, that for unknown reasons wont link the BN256G2 Library automatically for gm17 v1 and v2 contracts. I dont know why this is happening,
     //the contracts compile and deploy without any issue on remix. To fix this, the the BN256G2 Library must be compiled and deployed by itself, after that,
@@ -37,7 +37,7 @@ let jsonInterface = JSON.parse(solc.compile(jsonContractSource));
     if (format == "gm17") {
         let library = await deployLibrary();
         //replace lib placeholder with lib address in bytecode
-        bytecode.object = bytecode.object.replace(/\_\_\$[a-f0-9]{34}\$\_\_/g, library["_address"].replace("0x", ""))
+        bytecode.object = bytecode.object.replace(/\_\_\$[a-f0-9]{34}\$\_\_/g, library["_address"].replace("0x", ""));
     }
 
     let contract = new web3.eth.Contract(abi)
@@ -50,38 +50,38 @@ let jsonInterface = JSON.parse(solc.compile(jsonContractSource));
         })
         .on('receipt', (tx) => {
             if (tx.status == true) {
-                console.log("Contract Deployed! Gas used: " + tx.gasUsed)
+                console.log("Contract Deployed! Gas used: " + tx.gasUsed);
             }
         })
         .then(newContractInstance => {
             contract = newContractInstance;
-            Promise.all([makeTransaction(accounts[0], true), makeTransaction(accounts[0], false)])
+            Promise.all([makeTransaction(accounts[0], true), makeTransaction(accounts[0], false)]);
         })
         .catch(err => {
             console.log(err);
             process.exit(1);
-        })
+        });
 
     function makeTransaction(account, correct) {
         let proof = getProof(correct);
 
         function handleReceipt(tx) {
             if (tx.status == true && !correct) {
-                console.log("Verification has been successful with invalid proof data! THIS IS A BUG")
-                process.exit(1)
+                console.log("Verification has been successful with invalid proof data! THIS IS A BUG");
+                process.exit(1);
             }
 
             if (tx.status == true) {
-                console.log("Correct proof works! Gas used: " + tx.gasUsed)
+                console.log("Correct proof works! Gas used: " + tx.gasUsed);
             }
         }
 
         function handleError(err, correct) {
             if (!correct) {
-                console.log("False proof not verified! Success")
+                console.log("False proof not verified! Success");
             } else {
                 console.log(err);
-                process.exit(1)
+                process.exit(1);
             }
         }
 
@@ -90,46 +90,40 @@ let jsonInterface = JSON.parse(solc.compile(jsonContractSource));
                 .catch(handleError)
             :
             verifyTx_ABIV2(proof, account, correct).on('receipt', handleReceipt)
-                .catch(handleError)
+                .catch(handleError);
     }
 
     function verifyTx_ABIV2(proof, account, correct) {
+        var args = proof[0];
+        args = proof[1].length > 0 ? [args, proof[1]] : args;
 
-        var arguments = proof[0]
-        arguments = proof[1].length > 0 ? [arguments[0], proof[1]] : arguments
-
-        contract.methods.verifyTx(...arguments).send({
+        return contract.methods.verifyTx(...args).send({
             from: account,
             gas: 5000000
-        })
+        });
     }
 
     function verifyTx_ABIV1(proof, account, correct) {
-
-        var arguments = proof[0]
-        arguments = proof[1].length > 0 ? [...arguments, proof[1]] : arguments
+        var args = proof[0];
+        args = proof[1].length > 0 ? [...args, proof[1]] : args;
 
         return contract.methods.verifyTx(
-            ...arguments
+            ...args
         ).send({
             from: account,
             gas: 5000000
-        })
+        });
     }
 
     function getProof(correct) {
         let json = JSON.parse(fs.readFileSync(proofPath));
         let inputs = json["inputs"];
-        let proof = json["proof"]
+        let proof = json["proof"];
         //falsifies proof to check if verification fails
         if (!correct) {
             proof["a"][0] = "0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
         }
-        if (abiVersion == "v1") {
-            return [Object.values(proof), Object.values(inputs)];
-        } else if (abiVersion == "v2") {
-            return [proof, inputs]
-        }
+        return [Object.values(proof), Object.values(inputs)];
     }
 
     //function used for deploying BN256G2 Library, used for gm17 only
@@ -150,8 +144,8 @@ let jsonInterface = JSON.parse(solc.compile(jsonContractSource));
             },
         });
         let jsonInterfaceBin = JSON.parse(solc.compile(jsonContractSourceBin));
-        let abiLib = jsonInterfaceBin.contracts["BN256G2"]["BN256G2"].abi
-        let bytecodeLib = jsonInterfaceBin.contracts["BN256G2"]['BN256G2'].evm.bytecode
+        let abiLib = jsonInterfaceBin.contracts["BN256G2"]["BN256G2"].abi;
+        let bytecodeLib = jsonInterfaceBin.contracts["BN256G2"]['BN256G2'].evm.bytecode;
         return new web3.eth.Contract(abiLib)
             .deploy({
                 data: '0x' + bytecodeLib.object
@@ -165,6 +159,6 @@ let jsonInterface = JSON.parse(solc.compile(jsonContractSource));
                     console.log("Library couldn't be deployed");
                     process.exit(1);
                 }
-            })
+            });
     }
 })();
