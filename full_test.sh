af975cb71d49eb4af6f2749086f1552a63e7e98e
dark64 <darem966@gmail.com>
2021-05-26 17:52:25 +0200

    remove thread limit on cargo test, remove unnecessary -Zpackage-features

--- full_test.sh
+++ full_test.sh
@@ -4,7 +4,7 @@
 set -e
 
 if [ -n "$WITH_LIBSNARK" ]; then
-	cargo -Z package-features test --release --package zokrates_cli --features="libsnark" -- --ignored --test-threads=1
+	cargo test --release --package zokrates_cli --features="libsnark" -- --ignored
 else
-	cargo test --release -- --ignored --test-threads=1
+	cargo test --release -- --ignored
 fi
