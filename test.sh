1630b5af4d85fff215e1299b62a9ea1269bf8b9f
schaeff <thibaut@schaeff.fr>
2021-06-29 12:16:36 +0200

    fix bash

--- test.sh
+++ test.sh
@@ -6,6 +6,7 @@ set -e
 if [ -n "$WITH_LIBSNARK" ]; then
 	# run specifically the libsnark tests inside zokrates_core
 	cargo test --release --package zokrates_core --features="libsnark" libsnark
+fi
 
 # run all tests without libsnark on
 cargo test --release
