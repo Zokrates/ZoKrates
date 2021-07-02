df4add3dd387e3ab2199322bdb784d89f9d403f7
dark64 <darem966@gmail.com>
2021-07-02 14:51:16 +0200

    refactor ffi buffer, run libsnark tests consecutively

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -e
 
 if [ -n "$WITH_LIBSNARK" ]; then
 	# run specifically the libsnark tests inside zokrates_core
-	cargo test --release --package zokrates_core --features="libsnark" libsnark
+	cargo test --release --package zokrates_core --features="libsnark" libsnark -- --test-threads=1
 fi
 
 # run all tests without libsnark on
