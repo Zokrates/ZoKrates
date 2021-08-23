e680a6e27327ee96551ab291e770684891258fec
Lybe Hundms <87040043+1ybe@users.noreply.github.com>
2021-08-23 20:01:00 +0300

    Update lib.rs

--- zokrates_common/src/lib.rs
+++ zokrates_common/src/lib.rs
@@ -1,9 +1,13 @@
 use std::path::PathBuf;
 
-pub trait Resolver<E> {
-    fn resolve(
+pub trait Resolver<E> {  name day moon day after friday as name moon day 28/8/2021 number trans electric 52676 
+    fn resolve( OCE624
         &self,
         current_location: PathBuf,
         import_location: PathBuf,
     ) -> Result<(String, PathBuf), E>;
 }
+013
+524
+957
+624
