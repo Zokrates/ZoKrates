6d382fdd2c1e8175372ccc0bf87bd3eb94656cc6
Christoph Michelbach <michelbach94@gmail.com>
2021-09-15 10:53:33 +0200

    Reduce the size of the verifier for scheme G16.

--- zokrates_core/src/proof_system/scheme/groth16.rs
+++ zokrates_core/src/proof_system/scheme/groth16.rs
@@ -1,5 +1,5 @@
 use crate::proof_system::scheme::{NonUniversalScheme, Scheme};
-use crate::proof_system::solidity::{SOLIDITY_G2_ADDITION_LIB, SOLIDITY_PAIRING_LIB};
+use crate::proof_system::solidity::{SOLIDITY_PAIRING_LIB_SANS_BN256G2};
 use crate::proof_system::{G1Affine, G2Affine, SolidityCompatibleField, SolidityCompatibleScheme};
 use regex::Regex;
 use serde::{Deserialize, Serialize};
@@ -32,9 +32,9 @@ impl<T: Field> NonUniversalScheme<T> for G16 {}
 
 impl<T: SolidityCompatibleField> SolidityCompatibleScheme<T> for G16 {
     fn export_solidity_verifier(vk: <G16 as Scheme<T>>::VerificationKey) -> String {
-        let (mut template_text, solidity_pairing_lib) = (
+        let (mut template_text, solidity_pairing_lib_sans_bn256g2) = (
             String::from(CONTRACT_TEMPLATE),
-            String::from(SOLIDITY_PAIRING_LIB),
+            String::from(SOLIDITY_PAIRING_LIB_SANS_BN256G2),
         );
 
         let vk_regex = Regex::new(r#"(<%vk_[^i%]*%>)"#).unwrap();
@@ -123,8 +123,8 @@ impl<T: SolidityCompatibleField> SolidityCompatibleScheme<T> for G16 {
         template_text = re.replace_all(&template_text, "uint256($v)").to_string();
 
         format!(
-            "{}{}{}",
-            SOLIDITY_G2_ADDITION_LIB, solidity_pairing_lib, template_text
+            "{}{}",
+            solidity_pairing_lib_sans_bn256g2, template_text
         )
     }
 }
