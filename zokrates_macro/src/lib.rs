1943de1b595b7bb9e8aa44f4bc38e748676f5620
dark64 <darem966@gmail.com>
2021-10-08 13:37:42 +0200

    use cfg_if to avoid wasm compilation issues

--- zokrates_macro/src/lib.rs
+++ zokrates_macro/src/lib.rs
@@ -1,52 +1,64 @@
 extern crate proc_macro;
 
-use quote::quote;
-use syn::*;
-
-#[proc_macro_attribute]
-pub fn stopwatch(
-    _attr: proc_macro::TokenStream,
-    item: proc_macro::TokenStream,
-) -> proc_macro::TokenStream {
-    #[cfg(not(target_arch = "wasm32"))]
-    {
-        if let Ok(mut fun) = syn::parse::<ItemFn>(item.clone()) {
-            fun.block.stmts = rewrite_function(fun.sig.ident.to_string(), &mut fun.block.stmts);
-            return quote!(#fun).into();
+cfg_if::cfg_if! {
+    if #[cfg(feature = "wasm")] {
+        #[proc_macro_attribute]
+        pub fn stopwatch(
+            _attr: proc_macro::TokenStream,
+            item: proc_macro::TokenStream,
+        ) -> proc_macro::TokenStream {
+            item
         }
+    } else {
+        use quote::quote;
+        use syn::*;
 
-        if let Ok(mut fun) = syn::parse::<TraitItemMethod>(item.clone()) {
-            if let Some(block) = fun.default.as_mut() {
-                block.stmts = rewrite_function(fun.sig.ident.to_string(), &mut block.stmts);
+        #[proc_macro_attribute]
+        pub fn stopwatch(
+            _attr: proc_macro::TokenStream,
+            item: proc_macro::TokenStream,
+        ) -> proc_macro::TokenStream {
+            if let Ok(mut fun) = syn::parse::<ItemFn>(item.clone()) {
+                fun.block.stmts = rewrite_function(fun.sig.ident.to_string(), &mut fun.block.stmts);
                 return quote!(#fun).into();
             }
-        }
 
-        if let Ok(mut fun) = syn::parse::<ImplItemMethod>(item) {
-            fun.block.stmts = rewrite_function(fun.sig.ident.to_string(), &mut fun.block.stmts);
-            return quote!(#fun).into();
-        }
-
-        panic!("`zokrates_macro::stopwatch` should be used on functions")
-    }
-
-    #[cfg(target_arch = "wasm32")]
-    item
-}
-
-fn rewrite_function(name: String, statements: &mut Vec<Stmt>) -> Vec<Stmt> {
-    let mut block: Block = parse_quote! {{
-        pub struct Stopwatch(std::time::Instant);
-
-        impl Drop for Stopwatch {
-            fn drop(&mut self) {
-                log::debug!("Stopwatch: function `{}` was running for {}ms", #name, self.0.elapsed().as_millis());
+            if let Ok(mut fun) = syn::parse::<TraitItemMethod>(item.clone()) {
+                if let Some(block) = fun.default.as_mut() {
+                    block.stmts = rewrite_function(fun.sig.ident.to_string(), &mut block.stmts);
+                    return quote!(#fun).into();
+                }
             }
+
+            if let Ok(mut fun) = syn::parse::<ImplItemMethod>(item) {
+                fun.block.stmts = rewrite_function(fun.sig.ident.to_string(), &mut fun.block.stmts);
+                return quote!(#fun).into();
+            }
+
+            panic!("`zokrates_macro::stopwatch` should be used on functions")
         }
 
-        let _stopwatch = Stopwatch(std::time::Instant::now());
-    }};
+        fn rewrite_function(name: String, statements: &mut Vec<Stmt>) -> Vec<Stmt> {
+            let mut block: Block = parse_quote! {{
+                pub struct Stopwatch(std::time::Instant);
 
-    block.stmts.append(statements);
-    block.stmts
+                impl Drop for Stopwatch {
+                    fn drop(&mut self) {
+                        let location = core::panic::Location::caller();
+                        log::debug!("Stopwatch: function `{}` was running for {}ms ({}:{})",
+                            #name,
+                            self.0.elapsed().as_millis(),
+                            location.line(),
+                            location.column()
+                        );
+                    }
+                }
+
+                let _stopwatch = Stopwatch(std::time::Instant::now());
+            }};
+
+            block.stmts.append(statements);
+            block.stmts
+        }
+    }
 }
