9852b895e28a2f37cca08ded5edb59e13b3f9da1
schaeff <thibaut@schaeff.fr>
2023-01-26 15:42:42 +0100

    test

--- scripts/install_foundry.sh
+++ scripts/install_foundry.sh
@@ -3,5 +3,4 @@
 apt-get update -y
 apt-get install -y curl gnupg sudo build-essential git
 curl -L https://foundry.paradigm.xyz | bash
-export PATH="$PATH:/root/.foundry/bin"
-foundryup
+$HOME/.foundry/bin/foundryup
